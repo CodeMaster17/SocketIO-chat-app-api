215684804952270c76213e5fda710765218de801
Harshit <2105891@kiit.ac.in>
2022-09-26 07:49:36 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const { chats } = require("./data/data");
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    // res.send("API is running");
+    // res.send(chats);
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+})
+
+app.get("/api/chat/:id", (req, res) => {
+    // console.log("hello");
+    // console.log(req);
+    console.log(req.params.id);
+    // res.send("hello");
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 4000
+app.listen(4000, console.log(`server started on port ${PORT}`));
